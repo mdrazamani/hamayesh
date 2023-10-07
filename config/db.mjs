0332b8f956eee1d355dbc54a5171248c65c9a666
m.rezvani <mohsen.rezvani.rad33@gmail.com>
2023-10-07 12:14:19 +0330

    add nodemon and migrate collecition

--- config/db.mjs
+++ config/db.mjs
@@ -1,16 +1,47 @@
 import mongoose from "mongoose";
 import { dbURI } from "./index.mjs";
+import User from "../app/models/user.model.mjs";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default () => {
-    mongoose
-        .connect(dbURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("Connected to MongoDB");
-        })
-        .catch((error) => {
-            console.error("Error connecting to MongoDB:", error);
-        });
+  mongoose
+    .connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(async () => {
+      console.log("Connected to MongoDB");
+
+      // Dynamically import all models and create collections
+
+      const modelsPath = path.join(__dirname, "../app/models"); // Adjust the path accordingly
+      const modelFiles = fs
+        .readdirSync(modelsPath)
+        .filter((file) => file.endsWith(".mjs")); // Assuming your models are .mjs files
+
+      for (const file of modelFiles) {
+        const modelName = file.split(".")[0];
+        const model = await import(path.join(modelsPath, file));
+
+        if (model && model.default) {
+          mongoose.connection.db.createCollection(
+            modelName.toLowerCase(),
+            (err, collection) => {
+              if (err) {
+                console.error(`Error creating '${modelName}' collection:`, err);
+              } else {
+                console.log(`'${modelName}' collection created successfully`);
+              }
+            }
+          );
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
 };
