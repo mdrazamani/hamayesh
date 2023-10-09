2172e4675067f5db2cfd29a4e081e1abcd8a377d
mohammadreza <mdrazamani@gmail.com>
2023-10-09 12:10:30 +0330

    fix user model ...R

--- app/models/user.model.mjs
+++ app/models/user.model.mjs
@@ -3,14 +3,17 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema(
     {
         username: {
+            type: String, // اضافه کردن نوع داده
             required: true,
             unique: true,
             lowercase: true,
         },
         password: {
+            type: String, // اضافه کردن نوع داده
             required: true,
         },
         email: {
+            type: String, // اضافه کردن نوع داده
             required: true,
             unique: true,
             lowercase: true,
