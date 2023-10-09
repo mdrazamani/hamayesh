77cc7527d3eac5c14287707485793c53f42173bd
mohammadreza <mdrazamani@gmail.com>
2023-10-09 11:56:21 +0330

    update my last changes.

--- app.mjs
+++ app.mjs
@@ -3,15 +3,77 @@ import routes from "./routes/index.mjs";
 import dbconnect from "./config/db.mjs";
 import { PORT } from "./config/index.mjs";
 import errorHandler from "./app/middlewares/error.middleware.mjs";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./config/swagger.mjs";
+import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "./config/hpp.mjs";
+import xss from "xss-clean";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+import compression from "compression";
+import helmet from "helmet";
+import cors from "cors";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 dbconnect();
+
+app.enable("trust proxy");
+
+app.set("view engine", "pug");
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.set("views", path.join(`file://${path.join(__dirname, "../app/models")}`));
+
+// GLOBAL MIDDLEWARES
+app.use(cors());
+
+app.options("*", cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
+
+app.use(compression());
+
+// Swagger setup
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 app.use("/api/v1", routes);
 app.use(errorHandler);
 
-const port = PORT || 3000;
+const port = PORT || 8000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+//export default app;
--- config/db.mjs
+++ config/db.mjs
@@ -8,40 +8,47 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default () => {
-  mongoose
-    .connect(dbURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(async () => {
-      console.log("Connected to MongoDB");
+    mongoose
+        .connect(dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(async () => {
+            console.log("Connected to MongoDB");
 
-      // Dynamically import all models and create collections
+            // Dynamically import all models and create collections
 
-      const modelsPath = path.join(__dirname, "../app/models"); // Adjust the path accordingly
-      const modelFiles = fs
-        .readdirSync(modelsPath)
-        .filter((file) => file.endsWith(".mjs")); // Assuming your models are .mjs files
+            // const modelsPath = path.join(__dirname, "../app/models"); // Adjust the path accordingly
 
-      for (const file of modelFiles) {
-        const modelName = file.split(".")[0];
-        const model = await import(path.join(modelsPath, file));
+            // const modelFiles = fs
+            //     .readdirSync(modelsPath)
+            //     .filter((file) => file.endsWith(".mjs")); // Assuming your models are .mjs files
 
-        if (model && model.default) {
-          mongoose.connection.db.createCollection(
-            modelName.toLowerCase(),
-            (err, collection) => {
-              if (err) {
-                console.error(`Error creating '${modelName}' collection:`, err);
-              } else {
-                console.log(`'${modelName}' collection created successfully`);
-              }
-            }
-          );
-        }
-      }
-    })
-    .catch((error) => {
-      console.error("Error connecting to MongoDB:", error);
-    });
+            // for (const file of modelFiles) {
+            //     const { collectionName } = await import(
+            //         `file://${path.join(modelsPath, file)}`
+            //     );
+
+            //     if (collectionName) {
+            //         mongoose.connection.db.createCollection(
+            //             collectionName.toLowerCase(),
+            //             (err, collection) => {
+            //                 if (err) {
+            //                     console.error(
+            //                         `Error creating '${collectionName}' collection:`,
+            //                         err
+            //                     );
+            //                 } else {
+            //                     console.log(
+            //                         `'${collectionName}' collection created successfully`
+            //                     );
+            //                 }
+            //             }
+            //         );
+            //     }
+            // }
+        })
+        .catch((error) => {
+            console.error("Error connecting to MongoDB:", error);
+        });
 };
