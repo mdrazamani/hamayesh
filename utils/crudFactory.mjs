cebf0ec755aac05f2da4bed041f6e6b3429088ba
m.rezvani <mohsen.rezvani.rad33@gmail.com>
2023-10-16 20:51:59 +0330

    fix the pagination and other things

--- utils/crudFactory.mjs
+++ utils/crudFactory.mjs
@@ -4,10 +4,41 @@ import { getMessage } from "../config/i18nConfig.mjs";
 import constants from "./constants.mjs";
 import APIError from "./errors.mjs";
 import { paginate } from "./pagination.mjs";
+import { QueryBuilder } from "./QueryBuilder.mjs";
 
 export default {
-    getAll: (Model) => async (page, pageSize, query) => {
-        return await paginate(Model, page, pageSize, query);
+    getAll: (Model) => async (queryParams) => {
+        try {
+            // Destructure page and pageSize from queryParams and provide default values if not present
+            const { page = 1, pageSize = 10, ...otherParams } = queryParams;
+
+            // Create a new QueryBuilder instance. Here, we're passing an empty find query and the query parameters.
+            const queryBuilder = new QueryBuilder(Model, otherParams); // Assuming `User` is your Mongoose model.
+
+            // Apply the various query building methods for filtering, sorting, field selection, and pagination.
+            queryBuilder.filter().sort().limitFields().paginate(page, pageSize); // We assume your `paginate` method now accepts these parameters.
+
+            // Execute the query to get the paginated results.
+            const paginatedResults = await queryBuilder.query;
+            // Additionally, fetch the total number of documents matching the filter criteria.
+            const totalDocuments = await queryBuilder.totalDocuments();
+
+            // Structure the response object, including the items and necessary pagination metadata.
+            const response = {
+                items: paginatedResults, // The actual results.
+                total: totalDocuments, // Total number of matching documents.
+                pages: Math.ceil(totalDocuments / pageSize), // Total number of pages.
+                currentPage: page, // Current page number.
+                pageSize, // Number of results per page.
+            };
+
+            return response;
+        } catch (error) {
+            throw new APIError({
+                message: getMessage("errors.during_pagination"),
+                status: constants.BAD_REQUEST,
+            });
+        }
     },
     get: (Model) => async (id) => {
         const entity = await Model.findById(id);
