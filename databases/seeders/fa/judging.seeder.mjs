498ecd1bf67d9127d052702fb2f488b1cb7c9dd0
mohammadreza <mdrazamani@gmail.com>
2023-12-11 11:57:10 +0330

    judging seeder changes...

--- databases/seeders/fa/judging.seeder.mjs
+++ databases/seeders/fa/judging.seeder.mjs
@@ -3,6 +3,71 @@ import Article from "../../../app/models/article.model.mjs";
 import User from "../../../app/models/user.model.mjs";
 import { insertDocumentsDynamically } from "../../../config/modelChanger.mjs";
 
+const judgingStatus = ["accepted", "pending", "failed"];
+const rates = [
+    {
+        faTitle: "تناسب مقاله با موضوعات کنفرانس",
+        enTitle: "Suitability of the article to the conference topics",
+        slug: "s1",
+    },
+    {
+        faTitle: "کیفیت عنوان مقاله",
+        enTitle: "Quality of the article title",
+        slug: "s2",
+    },
+    {
+        faTitle: "تناسب عنوان با محتوا",
+        enTitle: "Suitability of the title with the content",
+        slug: "s3",
+    },
+    {
+        faTitle: "تازگی محتوا",
+        enTitle: "Freshness of the content",
+        slug: "s4",
+    },
+    {
+        faTitle: "کاربردی بودن محتوا",
+        enTitle: "Practicality of the content",
+        slug: "s5",
+    },
+    {
+        faTitle: "مستند بودن محتوا",
+        enTitle: "Documentary nature of the content",
+        slug: "s6",
+    },
+    {
+        faTitle: "کیفیت روش شناسی",
+        enTitle: "Quality of methodology",
+        slug: "s7",
+    },
+    {
+        faTitle: "کیفیت نمایش و بازنمایی مناسب یافته ها",
+        enTitle:
+            "Quality of presentation and proper representation of findings",
+        slug: "s8",
+    },
+    {
+        faTitle: "قابلیت استقبال از سوی مخاطبان کنفرانس",
+        enTitle: "Acceptability by conference audience",
+        slug: "s9",
+    },
+    {
+        faTitle: "ارزیابی کلی شما از مقاله",
+        enTitle: "Your overall assessment of the article",
+        slug: "s10",
+    },
+    {
+        faTitle: "تصمیم نهایی",
+        enTitle: "Final decision",
+        slug: "s11",
+    },
+    {
+        faTitle: "توضیحات، اصلاحات، و پیشنهادها",
+        enTitle: "Comments, revisions, and suggestions",
+        slug: "s12",
+    },
+];
+
 export const seedJudgingArticles = async () => {
     // Assuming articles and users are already seeded and we just need to fetch them.
     let articles;
@@ -10,7 +75,7 @@ export const seedJudgingArticles = async () => {
 
     try {
         articles = await Article.find();
-        referees = await User.find({ role: "referee" }); // Assuming a 'referee' role exists
+        referees = await User.find({ "role.name": "referee" }); // Assuming a 'referee' role exists
         if (articles.length === 0 || referees.length === 0) {
             throw new Error("Required articles or referees not found!");
         }
@@ -42,7 +107,7 @@ export const seedJudgingArticles = async () => {
     // Insert the sample judging articles into the database
     try {
         await JudgingArticle.deleteMany({});
-        await insertDocumentsDynamically(JudgingArticle, judgingArticlesData);
+        await JudgingArticle.insertMany(judgingArticlesData);
         console.log("Judging articles seeded successfully!");
     } catch (error) {
         console.error("Error seeding judging articles:", error);
